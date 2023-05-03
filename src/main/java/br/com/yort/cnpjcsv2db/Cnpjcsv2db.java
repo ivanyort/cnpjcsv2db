8d6f711c7ac0ef4cc4492afa146ffb85ea9f131e
Ivan Yort <ivan.yort@gmail.com>
2023-05-03 09:57:00 -0300

    Criação do repositório

---
+++ src/main/java/br/com/yort/cnpjcsv2db/Cnpjcsv2db.java
@@ -0,0 +1,16 @@
+/*
+by Ivan Yort (ivanyort@gmail.com)
+ */
+
+package br.com.yort.cnpjcsv2db;
+
+/**
+ *
+ * @author GPL
+ */
+public class Cnpjcsv2db {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
